8b4de627399505c777513e3abc0ac25fe40c4dbf
Elliot Nelson <Elliot.Nelson@hbo.com>
2018-09-03 09:53:06 -0400

    Revamp my level music and do some math tricks to squash it

--- src/js/Audio.js
+++ src/js/Audio.js
@@ -12,102 +12,128 @@
 class Audio {
     constructor() {
         this.ctx = new AudioContext();
-        this.tracks = {
-            intro: {
-                gain: 0,
-                gainNode: this.ctx.createGain(),
-                nextLoop: 0,
-                length: (29 + 3) / 5,
-                schedule: time => this.scheduleIntroTrack(this.tracks.intro.gainNode, time)
-            },
-            play: {
-                gain: 0,
-                gainNode: this.ctx.createGain(),
-                nextLoop: 0,
-                length: (29 + 3) / 5,
-                schedule: time => this.schedulePlayTrack(this.tracks.play.gainNode, time)
-            },
-            danger: {
-                gain: 0,
-                gainNode: this.ctx.createGain(),
-                nextLoop: 0,
-                length: (29 + 3) / 5,
-                schedule: time => this.scheduleDangerTrack(this.tracks.danger.gainNode, time)
-            },
-        };
 
-        this.tracks.intro.gainNode.connect(this.ctx.destination);
-        this.tracks.play.gainNode.connect(this.ctx.destination);
-        this.tracks.danger.gainNode.connect(this.ctx.destination);
+        this.gainNode = this.ctx.createGain();
+        this.gainNode.gain.value = 1;
+        this.gainNode.connect(this.ctx.destination);
+
+        this.tick = 0;
+        this.startOffset = this.ctx.currentTime;
+        this.tickLength = 1/5;
+        this.gain = 0.3;
     }
 
-    update() {
-        //this.tracks.intro.gain = Math.min(1, this.tracks.intro.gain + 0.01);
-        //this.tracks.play.gain = Math.min(0.1, this.tracks.play.gain + 0.005);
-        this.tracks.danger.gain = Math.min(0.8, this.tracks.danger.gain + 0.1);
+    update(delta) {
+        if (this.ctx.currentTime < time) {
+            // Let's "teleport forward" to the current time. This can happen if we're in
+            // a background tab for a while.
+            this.startOffset = time - this.ctx.currentTime;
+        }
 
-        this.updateTrack(this.tracks.intro);
-        this.updateTrack(this.tracks.play);
-        this.updateTrack(this.tracks.danger);
+        // Each frame, schedule some notes if we're less than a second away from when
+        // those notes need to be played.
+        let time = this.startOffset + this.tickLength * this.tick;
+        if (time - this.ctx.currentTime < 1) {
+            this.scheduleForTick(this.tick, this.tickLength, this.startOffset, this.gainNode);
+            this.tick++;
+        }
+
+        // Do some basic gain manipulation
+        console.log(delta);
+        if (game.menu || game.intro || game.player.dead || game.levelComplete) {
+            if (this.gain > 0.4) {
+                this.gain -= delta;
+            } else if (this.gain < 0.4) {
+                this.gain += delta;
+            }
+        } else {
+            if (this.gain < 0.8) {
+                this.gain += delta;
+            }
+        }
+        this.gainNode.gain.value = this.gain;
     }
 
-    updateTrack(track) {
-        track.gainNode.gain.value = track.gain;
-        if (track.nextLoop - this.ctx.currentTime < 1) {
-            track.schedule(track.nextLoop);
-            track.nextLoop = Math.max(track.nextLoop, this.ctx.currentTime) + track.length;
+    scheduleForTick(tick, tickLength, startOffset, dest) {
+        // I am not a music expert, so please assume that any statements I make about
+        // keys, chords, and other music theory in these comments may be 100% wrong.
+
+        // I experimented with more complicated 3- and 4-note chords, which
+        // sound good on the piano, but my experiments seemed to show that less-is-more
+        // when dealing with simple oscillators (you'd need a more complicated instrument,
+        // I think, for big min7 type chords to sound good).
+
+        // Overall, this is just a C minor chord, alternating fingers 1+3 and 1+5, with
+        // scattered dissonant (but not too dissonant) notes in between. Where I could, I
+        // tried to give it this odd start/stop feeling (like carnival music?) by alternating
+        // where the interstitial notes ended up (beat 3 vs beat 2/4).
+        let track = [
+            // Measure 1
+            [22, 19], // C Eb
+            [],
+            [],
+            [],
+            [22, 15], // C G
+            [21],     // C#
+            [],
+            [],
+            [22, 19], // C Eb
+            [],
+            [16],     // F#
+            [],
+            [22, 15], // C G
+            [],
+            [14],     // Ab
+            [],
+
+            // Measure 2
+            [22, 19], // C Eb
+            [],
+            [],
+            [16],     // F#
+            [22, 15], // C G
+            [],
+            [20],     // D
+            [],
+            [22, 19], // C Eb
+            [14],     // Ab
+            [],
+            [16],     // F#
+            [22, 15], // C G
+            [],
+            [21],     // C#
+            [20]      // D
+        ];
+
+        // Track 2 is Track 1 plus major 3rd (+4 notes), this transition sounds "normal", but
+        // makes the next transition a little more jarring.
+        let track2 = track.map(x => {
+            return x.map(y => y - 4);
+        });
+        // tweak: maj3rd -> min3rd sounds muddled unless we move the last couple notes a few higher
+        track2[30] = [16];
+        track2[31] = [15];
+
+        // Track 3 is Track 1 plus minor 3rd (+3 notes), a simple sinister transition.
+        let track3 = track.map((x, i) => {
+            return x.map(y => y - 3);
+        });
+
+        // Full 4 = our sequence in key of [C, C, E, Eb], repeat.
+        track = track.concat(track).concat(track2).concat(track3);
+
+        let notes = track[tick % track.length];
+
+        // Oscillator creation taken from the generator at https://xem.github.io/miniMusic/advanced.html,
+        // although I ended up moving the note creation out of the loop below, just so it is easier for
+        // me to reason about.
+        for (let i = 0; i < notes.length; i++) {
+            let o = this.ctx.createOscillator();
+            o.frequency.value = 988/1.06**notes[i];
+            o.type = 'triangle';
+            o.connect(dest);
+            o.start(startOffset + tickLength * tick);
+            o.stop(startOffset + tickLength * tick + tickLength * 1.33);
         }
     }
-
-    scheduleIntroTrack(dest, time) {
-        [10,10,10,10,16,16,19,19,22,22,22,22,22,22,22,22].map((v,i)=>{
-            let o = this.ctx.createOscillator();
-            let e = [1,9,17,25,13,29,5,21,1,5,9,13,17,21,25,29][i]/5;
-            o.frequency.value = 988/1.06**v;
-            o.connect(dest);
-            o.start(time + e);
-            o.stop(time + e + .2);
-        });
-    }
-
-    schedulePlayTrack(dest, time) {
-        [4,8,9,9,14,14,16,16,19,19,20,21,21,21,21,21,21,21,21,24,24].map((v,i) => {
-            let o = this.ctx.createOscillator();
-            let e = [27,7,6,23,3,11,3,27,11,19,22,2,4,10,12,15,18,20,23,15,26][i]/5;
-            o.frequency.value = 988/1.06**v;
-            o.type = 'triangle';
-            o.connect(dest);
-            o.start(time + e);
-            o.stop(time + e + .2);
-        });
-    }
-
-    scheduleDangerTrack(dest, time) {
-        [10,12,12,14,15,15,15,15,15,16,16,16,16,16,16,16,16,17,18,18,18,19,19,19,19,19,19,19,19,20,21,21,21,21,21,22,22,22,22,22,22,24,24].map((v,i) => {
-            let o = this.ctx.createOscillator();
-            let e = [21,20,22,5,1,11,15,19,21,3,5,6,9,12,16,20,22,23,3,20,22,1,6,7,14,18,19,21,23,24,2,4,7,8,25,1,3,17,18,19,20,8,24][i]/5;
-            o.frequency.value = 988/1.06**v;
-            o.type = 'triangle';
-            o.connect(dest);
-            o.start(time + e);
-            o.stop(time + e + .2);
-        });
-    }
 }
-
-/*
-
-//base song
-with(new AudioContext)[10,10,10,10,16,16,19,19,22,22,22,22,22,22,22,22].map((v,i)=>{with(createOscillator())v&&start(e=[1,9,17,25,13,29,5,21,1,5,9,13,17,21,25,29][i]/5,connect(destination),frequency.value=988/1.06**v)+stop(e+.2)})
-
-// base song with embellishments, triangle
-with(new AudioContext)[4,8,9,9,10,10,10,10,14,14,16,16,16,16,19,19,19,19,20,21,21,21,21,21,21,21,21,22,22,22,22,22,22,22,22,24,24].map((v,i)=>{with(createOscillator())v&&start(e=[27,7,6,23,1,9,17,25,3,11,3,13,27,29,5,11,19,21,22,2,4,10,12,15,18,20,23,1,5,9,13,17,21,25,29,15,26][i]/5,connect(destination),frequency.value=988/1.06**v,type='triangle',)+stop(e+.2)})
-
-// embellishments, triangle, no chords
-with(new AudioContext)[4,8,9,9,14,14,16,16,19,19,20,21,21,21,21,21,21,21,21,24,24].map((v,i)=>{with(createOscillator())v&&start(e=[27,7,6,23,3,11,3,27,11,19,22,2,4,10,12,15,18,20,23,15,26][i]/5,connect(destination),frequency.value=988/1.06**v,type='square',)+stop(e+.2)})
-
-// creepier? or dumber?
-with(new AudioContext)[10,12,12,14,15,15,15,15,15,16,16,16,16,16,16,16,16,17,18,18,18,19,19,19,19,19,19,19,19,20,21,21,21,21,21,22,22,22,22,22,22,24,24].map((v,i)=>{with(createOscillator())v&&start(e=[21,20,22,5,1,11,15,19,21,3,5,6,9,12,16,20,22,23,3,20,22,1,6,7,14,18,19,21,23,24,2,4,7,8,25,1,3,17,18,19,20,8,24][i]/5,connect(destination),frequency.value=988/1.06**v,type='triangle',)+stop(e+.2)})
-
-}
-*/
